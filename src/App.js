f7a742f5a099bfbf63ee824f8842c7cfd3f0e68e
Canberk <canberkkaya06@gmail.com>
2021-10-01 12:50:32 +0300

    api trigger

--- src/App.js
+++ src/App.js
@@ -1,129 +1,35 @@
-import { useEffect, useState } from 'react'
-import { API, graphqlOperation } from 'aws-amplify'
-import { createTodo } from './graphql/mutations'
-import { listTodos } from './graphql/queries'
-import {
-  AmplifyAuthenticator,
-  AmplifySignOut,
-  AmplifySignUp,
-  AmplifySignIn,
-} from '@aws-amplify/ui-react'
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
+import logo from './logo.svg'
+import './App.css'
 
-const initialState = { name: '', description: '' }
-
-const App = () => {
-  const [formState, setFormState] = useState(initialState)
-  const [todos, setTodos] = useState([])
-  //Auth states
-  const [authState, setAuthState] = useState()
-  const [user, setUser] = useState()
-
-  useEffect(() => {
-    return onAuthUIStateChange((nextAuthState, authData) => {
-      setAuthState(nextAuthState)
-      setUser(authData)
-    })
-  }, [])
-
-  useEffect(() => {
-    fetchTodos()
-  }, [])
-
-  function setInput(key, value) {
-    setFormState({ ...formState, [key]: value })
-  }
-
-  async function fetchTodos() {
-    try {
-      const todoData = await API.graphql(graphqlOperation(listTodos))
-      const todos = todoData.data.listTodos.items
-      setTodos(todos)
-    } catch (err) {
-      console.log('error fetching todos')
+function App() {
+  const sendRequest = async () => {
+    var URL = 'https://ae3xrmtxmk.execute-api.us-east-1.amazonaws.com/dev/item'
+    var otherParams = {
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: 'canberk',
+      }),
+      method: 'POST',
     }
-  }
 
-  async function addTodo() {
-    try {
-      if (!formState.name || !formState.description) return
-      const todo = { ...formState }
-      setTodos([...todos, todo])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, { input: todo }))
-    } catch (err) {
-      console.log('error creating todo:', err)
-    }
+    fetch(URL, otherParams)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        window.alert('TEBRIKLER')
+      })
+      .catch(function (err) {
+        window.alert(err)
+      })
   }
-
-  return authState === AuthState.SignedIn && user ? (
-    <div style={styles.container}>
-      <AmplifySignOut buttonText='Bye' />
-      <h2>Amplify Todos</h2>
-      <input
-        onChange={(event) => setInput('name', event.target.value)}
-        style={styles.input}
-        value={formState.name}
-        placeholder='Name'
-      />
-      <input
-        onChange={(event) => setInput('description', event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder='Description'
-      />
-      <button style={styles.button} onClick={addTodo}>
-        Create Todo
-      </button>
-      {todos.map((todo, index) => (
-        <div key={todo.id ? todo.id : index} style={styles.todo}>
-          <p style={styles.todoName}>{todo.name}</p>
-          <p style={styles.todoDescription}>{todo.description}</p>
-        </div>
-      ))}
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <button onClick={sendRequest}>Click</button>
+      </header>
     </div>
-  ) : (
-    <AmplifyAuthenticator usernameAlias='email'>
-      <AmplifySignUp
-        slot='sign-up'
-        usernameAlias='email'
-        formFields={[
-          { type: 'username' },
-          { type: 'password' },
-          { type: 'email' },
-        ]}
-      />
-      <AmplifySignIn slot='sign-in' />
-    </AmplifyAuthenticator>
   )
-}
-
-const styles = {
-  container: {
-    width: 400,
-    margin: '0 auto',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  todo: { marginBottom: 15 },
-  input: {
-    border: 'none',
-    backgroundColor: '#ddd',
-    marginBottom: 10,
-    padding: 8,
-    fontSize: 18,
-  },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: {
-    backgroundColor: 'black',
-    color: 'white',
-    outline: 'none',
-    fontSize: 18,
-    padding: '12px 0px',
-  },
 }
 
 export default App
